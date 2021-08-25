7cf5aabd3f5a02223bbc189e4dbab3eb909f21ba
BugraAkdagPXL <43740601+BugraAkdagPXL@users.noreply.github.com>
2021-08-25 13:37:21 +0200

    Delete herexamen directory

--- herexamen/app/src/main/java/com/example/herexamengarage/AppConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.herexamengarage;
-
-public class AppConstants {
-    public static final String ACTION_STOP = "stop";
-    public static final String ACTION_PAUSE = "pause";
-    public static final String ACTION_RESUME = "resume";
-    public static final String ACTION_START = "start";
-}
